facc40a8bc722335cf39b3bd55943d537029c283
Your Name <jignakalani007@gmail.com>
2025-06-24 16:22:21 +0100

    add calculation code

--- week-4/W4-S1-JavaScript/challenge/script.js
+++ week-4/W4-S1-JavaScript/challenge/script.js
@@ -1,12 +1,24 @@
-let num1 = prompt(" Enter number 1: ");
-let num2 = prompt("enter number 2: ");
-let operation = prompt(" enter your operation like '+' ,'-','*','/',");
+let num1 = Number(prompt("Enter number 1: "));
+let num2 = Number(prompt("Enter number 2: "));
+let operation = prompt("Enter your operation like '+', '-', '*', '/'");
 
-function calculate(num1,num2,operation){
-   
-    if (operation== "+"){
-        return num1+num2;
+function calculate(num1, num2, operation) {
+    if (operation === "+") {
+        return num1 + num2;
+    } else if (operation === "-") {
+        return num1 - num2;
+    } else if (operation === "*") {
+        return num1 * num2;
+    } else if (operation === "/") {
+        if (num2 !== 0) {
+            return num1 / num2;
+        } else {
+            return "Error: Division by zero.";
+        }
+    } else {
+        return "Invalid operation.";
     }
-    
 }
 
+let result = calculate(num1, num2, operation);
+alert("Result: " + result);
